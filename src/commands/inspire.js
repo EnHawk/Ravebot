777ebe10d2d9299429d0731d2a12fa8f7488675a
EnHawk <harits.rexus@gmail.com>
2022-12-07 17:44:06 +0700

    Fixed /inspire command.

--- src/commands/inspire.js
+++ src/commands/inspire.js
@@ -6,10 +6,10 @@ module.exports = {
     data: new Discord.SlashCommandBuilder()
         .setName(`inspire`)
         .setDescription(`Tells a motivational quotes`),
-    execute: async (i) => {
+    async execute (i) {
         const quote = await axios.get(`https://zenquotes.io/api/random`)
-            .then((res) => { return res.data })
-            .then((data) => { return `*${data[0].q}*\n-${data[0].a}` });
+            .then(res => { return res.data })
+            .then(data => { return `*${data[0].q}*\n-${data[0].a}` });
         const embed = new Discord.EmbedBuilder()
             .setDescription(quote)
             .setColor(`DarkButNotBlack`);
