a5e0978ad6f97d7d8d2345c3cb801f4d00759656
EnHawk <harits.rexus@gmail.com>
2022-12-10 06:27:06 +0700

    Added more definitions.

--- src/schemas/meta.js
+++ src/schemas/meta.js
@@ -1,14 +1,37 @@
-const id = {
+const str = {
+    type: String
+};
+const strUnique = {
+    type: String,
+    unique: true
+};
+const strRequired = {
+    type: String,
+    required: true
+};
+const strUniqueRequired = {
     type: String,
     unique: true,
     required: true
 };
-
 const number = {
     type: Number,
     default: 0
 };
+const numberUnique = {
+    type: Number,
+    unique: true
+};
+const numberRequired = {
+    type: Number,
+    required: true
+};
+const numberUniqueRequired = {
+    type: Number,
+    unique: true,
+    required: true
+}
 // Definitions
 
-module.exports = { id, number };
+module.exports = { str, strUnique, strRequired, strUniqueRequired, number, numberUnique, numberRequired, numberUniqueRequired };
 // Exports
