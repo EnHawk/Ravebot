f08b2025af3c07fa368711044ee5230e1c96c5e0
EnHawk <harits.rexus@gmail.com>
2022-12-09 20:16:55 +0700

    Added 'meta.js' for definitions.

---
+++ src/schemas/meta.js
@@ -0,0 +1,14 @@
+const id = {
+    type: String,
+    unique: true,
+    required: true
+};
+
+const number = {
+    type: Number,
+    default: 0
+}
+// Definitions
+
+module.exports = { id, number };
+// Exports
