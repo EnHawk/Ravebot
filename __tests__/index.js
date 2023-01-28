70ba63da57328ef97abf3bff5509aca3b1f9e708
Enlight Hawk <harits.rexus@gmail.com>
2023-01-28 04:05:46 +0000

    Some changes

---
+++ __tests__/index.js
@@ -0,0 +1,10 @@
+require(`@colors/colors`);
+// Import
+
+module.exports = {
+    connection: `MongoDB Connection`.underline,
+    listen: `Server Listen`.underline,
+    login: `Bot Login`.underline,
+    status: [ `Passing`.bgGreen, `Failing`.bgRed ]
+};
+// Export
--- __tests__/index.mjs
+++
@@ -1,7 +0,0 @@
-import chalk from "chalk";
-// Import
-
-export const connection = chalk.underline(`MongoDB Connection`);
-export const listen = chalk.underline(`Server Listen`);
-export const login = chalk.underline(`Bot Login`);
-export const status = [ chalk.bgGreen(`Passing`), chalk.bgRed(`Failing`) ];
